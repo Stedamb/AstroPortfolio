605ed148a7dd1bfc3f699409645fb15ed58c4dec
Ste <stedamb@gmail.com>
2025-10-19 11:26:01 +0200

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.stedamb.it',
   output: 'server',
   adapter: vercel(),
 
