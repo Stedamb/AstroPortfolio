867e84204df4b1f6ae7f871056a9961f2e43fdcd
Ste <stedamb@gmail.com>
2026-06-02 14:53:58 +0200

    performance: optimized images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,15 @@
+import animanoire from '@/assets/animanoire.webp';
+import blog from '@/assets/blog.webp';
+import videoJournal from '@/assets/video-journal.webp';
+import type { ImageMetadata } from 'astro';
+
 export type ProjectColor = 'pink' | 'green' | 'blue';
 export type ProjectBorder = `hover:border-${string}`;
 
 export interface Project {
   title: string;
   description: string;
-  image: string;
+  image: ImageMetadata;
   color: ProjectColor;
   border: ProjectBorder;
   github: string;
@@ -15,7 +20,7 @@ export const projects: Project[] = [
   {
     title: 'AnimaNoire Tattoo Atelier',
     description: 'Website for a tattoo shop. Built with Astro and Sanity CMS.',
-    image: '/animanoire.webp',
+    image: animanoire,
     color: 'pink',
     border: 'hover:border-pink-600',
     github: 'https://github.com/Stedamb/AnimaNoire/tree/astro-sanity',
@@ -24,7 +29,7 @@ export const projects: Project[] = [
   {
     title: 'Next Blog',
     description: 'My personal blog built with Next.js and Sanity CMS.',
-    image: '/blog.webp',
+    image: blog,
     color: 'green',
     border: 'hover:border-teal-600',
     github: 'https://github.com/Stedamb/next-blog',
@@ -33,7 +38,7 @@ export const projects: Project[] = [
   {
     title: 'Video Portfolio',
     description: 'A video portfolio showcasing my work and skills. Built with Astro.',
-    image: '/video-journal.webp',
+    image: videoJournal,
     color: 'blue',
     border: 'hover:border-cyan-600',
     github: 'https://github.com/Stedamb/Videomaker-portfolio/tree/dev',
